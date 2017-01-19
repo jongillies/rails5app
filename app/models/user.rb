654a01ebaee657596458ee82d03fa01e2e2c440c
Jon Gillies <jon@gillies.us>
2017-01-19 16:22:54 -0700

    remove default devise modules from model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # DEFAULT devise :database_authenticatable, :registerable,
+  # DEFAULT        :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable
+
 end
